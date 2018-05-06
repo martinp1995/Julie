98242f4b6d40855797c451ef9e13a13330526bc7
TheShooter <30504258+TheShooter71@users.noreply.github.com>
2018-05-06 13:31:36 +0200

    added time

--- fonctions/fonctions.js
+++ fonctions/fonctions.js
@@ -4,6 +4,8 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 var search = require('youtube-search');
 var ffmpeg = require("ffmpeg-binaries");
+var moment = require("moment");
+moment.locale("fr");
 
 var dispatcher;
 
@@ -27,7 +29,7 @@ const fonctions = {
         return queues[guild]
     },
 
-     play(msg, queue, song) {
+      play(msg, queue, song) {
         
      try {
 
@@ -54,10 +56,11 @@ const fonctions = {
                     "requested": msg.author.username,
                     "toplay": stream,
 		    "link": results[0].link,
-                    "publication": results[0].publishedAt,
+                    "publication": moment(results[0].publishedAt).format('LLL'),
                     "channelTitle": results[0].channelTitle,
                     "description": results[0].description,
                     "thumbnails": results[0].thumbnails.default.url,
+                    "videoId": results[0].id,
                 })
                 
             msg.channel.send(":satellite_orbital: **Ajout à la queue** - `" + queue[queue.length - 1].title + "`");
@@ -72,17 +75,22 @@ const fonctions = {
             
             if (!msg.guild.voiceConnection) return msg.channel.send(":x: Je ne suis pas connecté");
 
+    ytdl.getInfo(queue[0].link, (err, info) =>{
+console.log(info)
             let embed = new Discord.RichEmbed()
-        .setAuthor(`${client.user.username} - MUSIQUE`)
+        .setAuthor(queue[0].title)
         .setThumbnail(queue[0].thumbnails) 
         .setColor(0xFF0000)
         .addField(":bust_in_silhouette: Auteur", queue[0].channelTitle)
         .addField(":notepad_spiral: Description", (queue[0].description ? queue[0].description : "**Pas de description**"))
         .addField(":date: Date de publication", queue[0].publication)
+        .addField(":clock1: Durée", client.fonctions.time(info.length_seconds), true)
+        .addField(':eye: Nombre de vues', info.view_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true)
         .addField(":link: Lien", queue[0].link)
         .setFooter("demandé par @" + queue[0].requested);
-
+    
             msg.channel.send("**:headphones: En joue:**", embed);
+    })
 
             dispatcher = msg.guild.voiceConnection.playStream(queue[0].toplay)
 
@@ -113,7 +121,21 @@ const fonctions = {
   const colors = [0xED0505, 0xEDE605, 0x05ED52, 0x05AFED, 0xE605ED, 0xED0543, 0xC5C6C5, 0x111311];
   
   return colors[Math.floor(Math.random() * colors.length)];
-  }
+  },
+
+   time(temps){
+        let retour = "";
+        if (temps<60) {
+            retour = `${temps}s`;
+        } else if (temps<3600) {
+            retour = `${Math.floor(temps/60)}:${temps%60}`;
+        } else if (temps<86400) {
+            retour = `${Math.floor(temps/3600)}:${Math.floor(temps%3600/60)}:${temps%3600%60}`;
+        } else if (temps<604800) {
+            retour = `${Math.floor(temps/86400)}:${Math.floor(temps%86400/3600)}:${Math.floor(temps%86400%3600/60)}:${temps%86400%3600%60}`;
+        }
+        return retour;
+  },
 }
 
 module.exports = fonctions;
