ddccafcbafd44aa31805fed4a717f8a8f228b2b5
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-06-02 16:26:39 +0200

    wew

--- events/message.js
+++ events/message.js
@@ -6,9 +6,9 @@ if(msg.author.bot) return;
 if(msg.channel.type === "dm") return;
 
 if (!msg.content.startsWith(prefix)) return; 
-
-  let args = msg.content.toLowerCase().split(" ").slice(1);
+  
   let cmd = msg.content.slice(prefix.length).trim().split(' ').shift().toLowerCase(); 
   let commande = client.commands.get(cmd);
+  
   if(commande) commande.run(client,msg,prefix);
 }
